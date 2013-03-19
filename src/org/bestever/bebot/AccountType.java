eb388a7beffc9ab1931058d0c5d007730f99fd05
Water_Melon <watermelonst@hotmail.com>
2013-03-19 14:10:43 -0400

    Updated account system to resemble an integer that acts as a bit mask

--- src/org/bestever/bebot/AccountType.java
+++ src/org/bestever/bebot/AccountType.java
@@ -1,88 +1,25 @@
 package org.bestever.bebot;
 
-/**
- * This contains an enumeration that is used for determining account status when 
- * people try to host with the bot
- */
-public enum AccountType {
-	GUEST (0, "guest"), 
-	TRUSTED (1, "trusted"), 
-	MODERATOR (2, "moderator"),
-	ADMIN (3, "admin"); 
-    
+public class AccountType {
+	
 	/**
-	 * This contains the index of the account (its a constant really)
+	 * These are generic unknown accounts that have just registered
 	 */
-	private final int account_type;
-    
+	public static final int GUEST = 0; // 0
+	
 	/**
-	 * The account name is stored here for easy reference/string output
+	 * Admin users have access to everything
 	 */
-	private final String account_type_name;
-    
+	public static final int ADMIN = 1 << 0; // 1
+	
 	/**
-	 * This initializes the enumeration types
-	 * @param type The index of the account
-	 * @param name A string containing the name
+	 * Moderators are allowed to do various additional functions most users cant
 	 */
-	AccountType(int type, String name) {
-		this.account_type = type;
-		this.account_type_name = name;
-	}
-    
+	public static final int MODERATOR = 1 << 1; // 2
+	
 	/**
-	 * Returns the constant index of the type
-	 * @return An integer represeting a constant of the class
+	 * Trusted users may have access to things most users don't have access to (ex: IRC bots...etc)
 	 */
-	int getAccountIndex() {
-		return this.account_type;
-	}
-    
-	/**
-	 * Used to retrieve the account name associated with the enumeration
-	 * @return A string containing a lower case name
-	 */
-	String getAccountName() {
-		return this.account_type_name;
-	}
-    
-	/**
-	 * This will return the object if you have the enumeration index number
-	 * @param index The number constant
-	 * @return The object type that matches up with the index
-	 */
-	AccountType getAccountTypeFromNumber(int index) {
-		switch (index) {
-			case 3:
-				return ADMIN;
-			case 2:
-				return MODERATOR;
-			case 1:
-				return TRUSTED;
-			case 0: // Guests should act like the default, fall through
-			default:
-				return GUEST;
-		}
-	}
-    
-	/**
-	 * This will return the object if you have the enumeration index number
-	 * @param name The name to check
-	 * @return The object type that matches up with the index
-	 */
-	AccountType getAccountTypeFromName(String name) {
-		switch (name.toLowerCase()) {
-			case "admin":
-			case "administrator":
-				return ADMIN;
-			case "mod":
-			case "moderator":
-				return MODERATOR;
-			case "trusted":
-				return TRUSTED;
-			case "guest": // Guests should act like the default, fall through
-			default:
-				return GUEST;
-		}
-	}
+	public static final int TRUSTED = 1 << 2; // 4
+	
 }
