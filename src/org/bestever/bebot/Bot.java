19cb9889de716da5c0d071d67a1a393837440ef7
Water_Melon <watermelonst@hotmail.com>
2013-04-10 13:36:30 -0400

    Removed .reflect since we have anything we need anyways (was great for testing while it existed)

--- src/org/bestever/bebot/Bot.java
+++ src/org/bestever/bebot/Bot.java
@@ -275,9 +275,6 @@ public class Bot extends PircBot {
 				case ".slot":
 					processSlot(userLevel, keywords);
 					break;
-				case ".reflect":
-					processReflect(userLevel, keywords);
-					break;
 				case ".userlevel":
 					processUserLevel(userLevel, hostname);
 					break;
@@ -543,42 +540,6 @@ public class Bot extends PircBot {
 		if (isAccountTypeOf(userLevel, ADMIN)) {
 			this.disconnect();
 			System.exit(0);
-		}
-	}
-	
-	/**
-	 * This is a debug function to get fields, it will iterate through the keywords and get methods.
-	 * This method is not complete at all yet; also very messy
-	 * @param userLevel The user's bitmask level
-	 * @param keywords The keywords passed to the reflect function (keyword[0] should be ".reflect")
-	 */
-	private void processReflect(int userLevel, String[] keywords) {
-		if (isAccountTypeOf(userLevel, ADMIN)) {
-			if (keywords.length < 3) {
-				sendMessage(cfg_data.irc_channel, ".reflect usage: .reflect <port> <field/class> [subfield/subclass...]");
-				return;
-			}
-			// Ensure it is a valid port
-			if (!Functions.isNumeric(keywords[1])) {
-				sendMessage(cfg_data.irc_channel, "Invalid port number (" + keywords[1] + ").");
-				return;
-			}
-			
-			// Search the port
-			int port = Integer.parseInt(keywords[1]);
-			Server server = getServer(port);
-			try {
-				Field targetField = server.getClass().getDeclaredField(keywords[2]);
-				try {
-					sendMessage(cfg_data.irc_channel, "Reflected (" + port + ") [ " + keywords[2] + " = Field: " + targetField.get(server).toString() + " ]");
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-					e.printStackTrace();
-				}
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (NoSuchFieldException nsfe) {
-				sendMessage(cfg_data.irc_channel, "No such field to reflect.");
-			}
 		}
 	}
 
