330d901f00ef459039f75561cc4c2616769493b5
IvanTopolcic <topolcic.ivan@gmail.com>
2013-07-09 21:28:04 -0400

    Fixed skill and getting skill from server

--- src/org/bestever/bebot/ServerProcess.java
+++ src/org/bestever/bebot/ServerProcess.java
@@ -116,7 +116,9 @@ public class ServerProcess extends Thread {
 		}
 
 		if (server.skill > -1)
-			runCommand.add("+skill" + server.skill);
+			runCommand.add("+skill " + server.skill);
+		else
+			server.skill = 4;
 		
 		if (server.gamemode != null)
 			runCommand.add("+" + server.gamemode + " 1");
