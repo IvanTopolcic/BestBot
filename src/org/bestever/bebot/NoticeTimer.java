eb948241a69c04e650476a4bc9d20ca1b2573e1b
Ivan Topolcic <topolcic.ivan@gmail.com>
2013-08-05 02:24:57 -0400

    Forgot to add NoticeTimer.java to git (welp)

---
+++ src/org/bestever/bebot/NoticeTimer.java
@@ -0,0 +1,31 @@
+package org.bestever.bebot;
+
+import java.util.TimerTask;
+
+/**
+ * Class that runs timed broadcasts (notices, etc)
+ */
+public class NoticeTimer extends TimerTask {
+
+	/**
+	 * Holds the bot
+	 */
+	private Bot bot;
+
+	/**
+	 * Constructor
+	 * @param bot Bot - the main bot object
+	 */
+	public NoticeTimer(Bot bot) {
+		this.bot = bot;
+	}
+
+	/**
+	 * Send a message to all servers
+	 */
+	public void run() {
+		for (Server s : bot.servers) {
+			s.in.println("say " + bot.cfg_data.bot_notice);
+		}
+	}
+}
